55feee5a9f32448d9ba8768e86d4c00267b80f97
arincdemir <68223368+arincdemir@users.noreply.github.com>
2024-05-09 23:12:14 +0300

    added home page and manager login

--- volleydb/app/urls.py
+++ volleydb/app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name='home'),
+    path("managerLogin/", views.databaseManagerLogin, name='managerLogin'),
+    path("login2/", views.login2, name='login2'),
+    path("manager/", views.manager, name='manager'),
 ]
